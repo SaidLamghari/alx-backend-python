a8242d2e861e37d119dbce0ac35851218c239532
SaidLamghari <said.lmg@gmail.com>
2024-07-04 22:12:36 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module pour définir des variables avec annotations
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
