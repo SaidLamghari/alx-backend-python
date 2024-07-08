e84f4c72e4e01838a5039273c76f8189d9422b2b
SaidLamghari <said.lmg@gmail.com>
2024-07-08 16:18:55 +0000

    0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Coroutine asynchrone `wait_random` qui attend
+un délai aléatoire entre 0 et max_delay secondes.
+
+Utilisation :
+    import asyncio
+
+    wait_random = __import__('0-basic_async_syntax').wait_random
+
+    print(asyncio.run(wait_random())# Exécute avec max_delay par défaut = 10
+    print(asyncio.run(wait_random(5)))# Exécute avec max_delay = 5
+    print(asyncio.run(wait_random(15)))# Exécute avec max_delay = 15
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    Coroutine asynchrone qui attend un délai
+    aléatoire entre 0 et max_delay secondes.
+
+    Args:
+        max_delay (float): Nombre maximum de
+        secondes à attendre (par défaut 10).
+
+    Returns:
+        float: Le délai aléatoire qui a été attendu.
+    """
+    # Génère un délai aléatoire
+    valdelay = random.uniform(0, max_delay)
+    # Attend de manière asynchrone le délai
+    await asyncio.sleep(valdelay)
+    # Retourne le délai une fois complété
+    return valdelay
