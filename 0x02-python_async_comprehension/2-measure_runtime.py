0a8b9055aa7d281337f3999f7be5c97fe3d8e06c
SaidLamghari <said.lmg@gmail.com>
2024-07-09 16:16:40 +0000

    2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Auteur SAID LAMGHARI
+"""
+
+import asyncio
+import time
+# Assurez-vous que le chemin d'importation est correct
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Mesure le temps total d'exécution de async_comprehension
+    lorsqu'il est exécuté quatre fois en parallèle.
+
+    Returns:
+        float: Temps total d'exécution en secondes.
+    """
+    start_tme = time.perf_counter()
+    # Utilisation de asyncio.gather pour exécuter
+    # async_comprehension quatre fois en parallèle
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_tme = time.perf_counter()
+    return end_tme - start_tme
