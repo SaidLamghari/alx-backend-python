ce7716a9c16996613b5386e59640aa29d63804bc
SaidLamghari <said.lmg@gmail.com>
2024-07-29 17:46:02 +0000

    1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 0. Parameterize a unit test
+1. Parameterize a unit test
 Auteur SAID LAMGHARI
 """
 import unittest
@@ -27,6 +28,26 @@ class TestAccessNestedMap(unittest.TestCase):
         :param expected_result: Le résultat attendu.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError"),
+        ({"a": 1}, ("a", "b"), "KeyError"),
+    ])
+    def test_access_nested_map_exception(self, nested_map,
+                                         path, expected_exception):
+        """
+        Teste que la fonction access_nested_map
+        lève une exception KeyError avec le message attendu.
+
+        :param nested_map: La carte imbriquée à tester.
+        :param path: Le chemin pour accéder à la valeur.
+        :param expected_exception: Le type d'exception attendu.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Vérifie que le message de l'exception est le message attendu
+        self.assertEqual(str(context.exception), expected_exception)
 
 
 if __name__ == "__main__":
